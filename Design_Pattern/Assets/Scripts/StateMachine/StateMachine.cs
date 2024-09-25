8a80125adb1b6e6d8e9db513aef1db846dcbefdb
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-09-25 17:48:16 +0900

    상태 머신 공부

--- Design_Pattern/Assets/Scripts/StateMachine/StateMachine.cs
+++ Design_Pattern/Assets/Scripts/StateMachine/StateMachine.cs
@@ -8,27 +8,73 @@ using UnityEngine;
 public class StateMachine : MonoBehaviour
 {
     public IState currentState {  get; private set; }
-    public IState CurrentState { get; private set; }
     public WalkState walkState;
     public JumpState jumpState;
     public IdleState idleState;
     public void Initialize(IState startingState)
     {
-        CurrentState = startingState;
+        currentState = startingState;
         startingState.Enter();
     }
     public void TransitionTo(IState nextState)
     {
-        CurrentState.Exit();
-        CurrentState = nextState;
+        currentState.Exit();
+        currentState = nextState;
         nextState.Enter();
     }
     public void Update()
     {
-        if (CurrentState != null)
+        if (currentState != null)
         {
-            CurrentState.Update();
+            currentState.Update();
         }
     }
-
+    public StateMachine(PlayerController player)
+    {
+        this.walkState = new WalkState(player);
+        this.jumpState = new JumpState(player);
+        this.idleState = new IdleState(player);
+    }
 }
+public enum PlayerControllerState
+{
+    Idle,
+    Walk,
+    Jump
+}
+public class PlayerController : MonoBehaviour
+{
+    private StateMachine stateMachine;
+
+    private void Awake()
+    {
+        stateMachine = new StateMachine(this);
+        stateMachine.Initialize(stateMachine.idleState); // 초기 상태 설정
+    }
+
+    private void Update()
+    {
+        GetInput();
+        stateMachine.Update(); // 상태 기계의 업데이트 호출
+    }
+
+    private void GetInput()
+    {
+        // 입력 처리 로직
+
+        // 상태 전환 예시
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            stateMachine.TransitionTo(stateMachine.walkState);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            stateMachine.TransitionTo(stateMachine.jumpState);
+        }
+        else if (Input.GetKeyDown(KeyCode.None))
+        {
+            stateMachine.TransitionTo(stateMachine.idleState);
+        }
+    }
+}
+
--- Design_Pattern/Assets/Scripts/StateMachine/WalkState.cs
+++ Design_Pattern/Assets/Scripts/StateMachine/WalkState.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkState : MonoBehaviour
+public class WalkState : IState
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerController player;
+    public WalkState(PlayerController player)
     {
-        
+        this.player = player;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Enter()
     {
-        
+        // 상태에 처음 진입할 때 실행되는 코드
+    }
+    public void Update()
+    {
+        // 조건이 다른 상태로 전환하기 위해
+        // 존재하는지 여부를 감지하기 위한 로직 추가
+    }
+    public void Exit()
+    {
+        // 상태에서 벗어날 때 실행되는 코드
     }
 }
